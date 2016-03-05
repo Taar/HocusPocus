ca051d1b60504710c715bafca8e78c1bfec93bae
Randy Topliffe <randytopliffe@gmail.com>
2016-03-05 17:29:57 -0500

    Update run file to use a ini file for file paths

--- hocuspocus/run.py
+++ hocuspocus/run.py
@@ -1,11 +1,23 @@
+import argparse
+import configparser
 from main import main
 from door_controller import DoorController
 import Adafruit_BBIO.GPIO as GPIO
 
-door_controller = DoorController(GPIO)
 
-main(
-    '/tmp/door_controller.pid',
-    '/tmp/door_controller_error.txt',
-    door_controller
-)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Door controller daemon')
+    parser.add_argument('ini_file', type=argparse.FileType('r'))
+    args = parser.parse_args()
+
+    config = configparser.ConfigParser()
+    config.read_file(args.ini_file)
+    paths = config.get('paths')
+
+    door_controller = DoorController(GPIO)
+
+    main(
+        paths.pid_file,
+        paths.error_file,
+        door_controller
+    )
